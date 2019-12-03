01690283591c55b9143fb100e2f245c82bd153c3
dglav <glavich_drew@yahoo.com>
2019-12-03 22:36:44 +0900

    Updates

---
+++ precache-manifest.151697df89b3e96dcf52c6575543ddc6.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7f8fdaebcf595361860fac28259d0be7",
+    "url": "/hackernoon-viewer/index.html"
+  },
+  {
+    "revision": "9f9dd55f0ab85a366531",
+    "url": "/hackernoon-viewer/static/css/main.5b1deeec.chunk.css"
+  },
+  {
+    "revision": "fdeef22e99341df8b960",
+    "url": "/hackernoon-viewer/static/js/2.06b9f17a.chunk.js"
+  },
+  {
+    "revision": "9f9dd55f0ab85a366531",
+    "url": "/hackernoon-viewer/static/js/main.260cef23.chunk.js"
+  },
+  {
+    "revision": "47eb6015103f12084a6f",
+    "url": "/hackernoon-viewer/static/js/runtime-main.1dd65b24.js"
+  }
+]);
